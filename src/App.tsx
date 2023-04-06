d6d153b4bdb82da9be9b1e8aec823f67b3eb34ee
KHC <the2sang@gmail.com>
2023-04-07 07:40:21 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect, useState, useMemo, useRef, MouseEvent, KeyboardEvent} from 'react';
+
+
+interface User {
+  username: string,
+  age: number
+}
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n < 2 ) return n
+  return fib(n - 1) + fib(n - 2)
+}
+
+const myNum: number = 37
 
 function App() {
+
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users:', users)
+
+    return () => console.log('unmounting')
+  }, [users])
+
+  const addTwo = useCallback(
+    (e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => setCount(prev => prev + 1), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+
     </div>
   );
 }
